191f162417bab446734fe0a9acf8b94a35629a95
christopouloumarianna <50484642+christopouloumarianna@users.noreply.github.com>
2019-05-16 09:37:42 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ironas
+{
+
+
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("give a=");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("give b=");
+        double b = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("give c=");
+        double c = double.Parse(Console.ReadLine());
+
+        double r = (a + b + c) / 2;
+
+        double E = Math.Sqrt(r * (r - a) * (r - b) * (r - c));
+
+
+
+        Console.WriteLine("E= {0}", E);
+
+        Console.ReadLine();
+    }
+
+
+
+
+}
+
+
